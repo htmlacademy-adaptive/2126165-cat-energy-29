dbf1c3004034b3bb3928807a79ec85bfd3aed217
Ксения <easyghoul@gmail.com>
2023-08-14 19:14:16 +0700

    Исправляет ошибки линтера

--- source/js/map.js
+++ source/js/map.js
@@ -1,25 +1,25 @@
 let center = [59.93871181590914, 30.3230150423278];
 
 function init() {
-	let map = new ymaps.Map('map', {
-		center: center,
-		zoom: 17
-	});
+  let map = new ymaps.Map("map", {
+    center: center,
+    zoom: 17
+  });
 
   let placemark = new ymaps.Placemark(center, {}, {
-		iconLayout: 'default#image',
-		iconImageHref: '../img/pin.svg',
-		iconImageSize: [57, 53],
-		iconImageOffset: [-19, -44]
-	});
+    iconLayout: "default#image",
+    iconImageHref: "../img/pin.svg",
+    iconImageSize: [57, 53],
+    iconImageOffset: [-19, -44]
+  });
 
-	map.controls.remove('geolocationControl');
-  map.controls.remove('searchControl');
-  map.controls.remove('trafficControl');
-  map.controls.remove('typeSelector');
-  map.controls.remove('fullscreenControl');
-  map.controls.remove('zoomControl');
-  map.controls.remove('rulerControl');
+  map.controls.remove("geolocationControl");
+  map.controls.remove("searchControl");
+  map.controls.remove("trafficControl");
+  map.controls.remove("typeSelector");
+  map.controls.remove("fullscreenControl");
+  map.controls.remove("zoomControl");
+  map.controls.remove("rulerControl");
 
   map.geoObjects.add(placemark);
 }
