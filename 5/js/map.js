8a05c3315350e27330a741aaf777e79450561182
Keks <keks@htmlacademy.ru>
2023-08-15 12:07:40 +0300

    :heavy_check_mark: Сборка #5

--- 5/js/map.js
+++ 5/js/map.js
@@ -1,15 +1,32 @@
-let center = [59.93871181590914, 30.3230150423278];
+let screenWidth = window.screen.width;
+let center;
+let zoom;
+let pinSize;
+
+if (screenWidth < 1280) {
+  center = [59.938631, 30.323037];
+  zoom = 15;
+} else if (screenWidth >= 1280) {
+  center = [59.939478, 30.309472];
+  zoom = 14;
+};
+
+if (screenWidth < 768) {
+  pinSize = [57, 53];
+} else {
+  pinSize = [113, 106];
+}
 
 function init() {
   let map = new ymaps.Map("map", {
     center: center,
-    zoom: 14
+    zoom: zoom
   });
 
-  let placemark = new ymaps.Placemark(center, {}, {
+  let placemark = new ymaps.Placemark([59.93871181590914, 30.3230150423278], {}, {
     iconLayout: "default#image",
     iconImageHref: "./img/pin.svg",
-    iconImageSize: [57, 53],
+    iconImageSize: pinSize,
     iconImageOffset: [-19, -44]
   });
 
@@ -22,6 +39,6 @@ function init() {
   map.controls.remove("rulerControl");
 
   map.geoObjects.add(placemark);
-}
+};
 
 ymaps.ready(init);
