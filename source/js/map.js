050c8beabe52828b99d8e6885a10dea85ff55c1b
Ксения <easyghoul@gmail.com>
2023-08-15 09:16:19 +0700

    Убирает с карты элементы управления

--- source/js/map.js
+++ source/js/map.js
@@ -1,12 +1,10 @@
-let center = [59.93871181590914, 30.3230150423278];
-
 function init() {
   let map = new ymaps.Map("map", {
-    center: center,
-    zoom: 14
+    center: [59.938572, 30.319937],
+    zoom: 17
   });
 
-  let placemark = new ymaps.Placemark(center, {}, {
+  let placemark = new ymaps.Placemark([59.93871181590914, 30.3230150423278], {}, {
     iconLayout: "default#image",
     iconImageHref: "./img/pin.svg",
     iconImageSize: [57, 53],
