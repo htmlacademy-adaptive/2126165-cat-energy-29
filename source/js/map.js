c87544fb6c38886ff0ea057f951d080644acc66a
Ксения <easyghoul@gmail.com>
2023-08-16 10:47:06 +0700

    Добавляет перезагрузку страницы при изменении масштаба экрана

--- source/js/map.js
+++ source/js/map.js
@@ -42,3 +42,13 @@ function init() {
 };
 
 ymaps.ready(init);
+
+window.onresize = function (event) {
+  if (document.documentElement.clientWidth < 768) {
+    location.reload()
+  } else if (document.documentElement.clientWidth >= 768) {
+    location.reload()
+  } else if (document.documentElement.clientWidth >= 1280) {
+    location.reload()
+  };
+}
